8ce9000646861777f519c416eb664b677473d5b6
rmamdouh <rmamdouh.ese@gmail.com>
2015-01-17 16:39:59 -0400

    Modified the Transaction table columns and updated its form. Added select boxes to the new transaction page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150116092245) do
+ActiveRecord::Schema.define(:version => 20150117203000) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -22,11 +22,13 @@ ActiveRecord::Schema.define(:version => 20150116092245) do
   end
 
   create_table "transactions", :force => true do |t|
-    t.integer  "from"
-    t.integer  "to"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
     t.integer  "amount"
+    t.integer  "first_account_id"
+    t.string   "first_direction"
+    t.integer  "second_account_id"
+    t.string   "second_direction"
   end
 
 end
